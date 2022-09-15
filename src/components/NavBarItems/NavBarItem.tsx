bf7f44c141a6369797e59171ca3fb9ade937fd94
Cesar Munguia <c.munguiabrito@gmail.com>
2022-09-15 14:46:46 -0500

    NavBar config

--- src/components/NavBarItems/NavBarItem.tsx
+++ src/components/NavBarItems/NavBarItem.tsx
@@ -1,6 +1,11 @@
 import React from "@astrojs/react";
 
-export function NavBarItem() {
+interface NavBarItemProps {
+  titulo: string;
+  enlace?: string;
+}
+
+export function NavBarItem({ titulo, enlace }: NavBarItemProps) {
   const cssClasses = {
     active:
       "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white",
@@ -10,8 +15,12 @@ export function NavBarItem() {
 
   return (
     <li>
-      <a href="#" className={cssClasses.active} aria-current="page">
-        Inicio
+      <a
+        href={`./${enlace}`}
+        className={cssClasses.inactive}
+        aria-current="page"
+      >
+        {titulo}
       </a>
     </li>
   );
--- src/components/NavBarItems/NavBarWrapper.tsx
+++ src/components/NavBarItems/NavBarWrapper.tsx
@@ -1,5 +1,19 @@
 import React from "@astrojs/react";
+import { NavBarItem } from "./NavBarItem";
 
+const menu = [
+  { enlace: "", titulo: "Inicio" },
+  { enlace: "guias", titulo: "Guias" },
+  { enlace: "planificador", titulo: "Planificador" },
+];
 export function NavBarWrapper() {
-  return <div>NavBarWrapper</div>;
+  return (
+    <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      {menu.map((item) => (
+        <>
+          <NavBarItem {...item} />
+        </>
+      ))}
+    </ul>
+  );
 }
