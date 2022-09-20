7f60d86866e81cbfe95478e7b1ac208a76e17f85
Cesar Munguia <c.munguiabrito@gmail.com>
2022-09-20 09:34:35 -0500

    URL Fixes

--- src/components/NavBarItems/NavBarItem.tsx
+++ src/components/NavBarItems/NavBarItem.tsx
@@ -1,4 +1,5 @@
 import React from "@astrojs/react";
+import "./NavBarItems.css";
 
 interface NavBarItemProps {
   titulo: string;
@@ -7,10 +8,8 @@ interface NavBarItemProps {
 
 export function NavBarItem({ titulo, enlace }: NavBarItemProps) {
   const cssClasses = {
-    active:
-      "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white",
-    inactive:
-      "block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
+    active: "elementMenuActive",
+    inactive: "elementMenu",
   };
 
   return (
